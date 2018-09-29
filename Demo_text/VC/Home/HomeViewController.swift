d344c7dbb670f08b17cb4c754000c535af988c28
sunsun <1294205031@qq.com>
2018-09-29 11:47:15 +0800

    snpkit

--- Demo_text/VC/Home/HomeViewController.swift
+++ Demo_text/VC/Home/HomeViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
+import SnapKit
+
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
